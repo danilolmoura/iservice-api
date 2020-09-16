9d6b77863a58117018ed77ce8d4b139f22bbdce5
Danilo Moura <danilolmoura@gmail.com>
2020-09-16 20:11:01 -0300

    Add create products tests

--- tests/funcional/test_resource_product.py
+++ tests/funcional/test_resource_product.py
@@ -1,11 +1,12 @@
 import json
 import pdb
 
-from application.models import Product
+from application.models import Product, Store, User
 from tests import utils
 
 class TestProductResource():
     url_product = '/api/v1/product'
+    url_store = '/api/v1/store'
     url_product_item = '/api/v1/product/{}'
     url_change_status = '/api/v1/product/{}/status'
     url_request_exchange = '/api/v1/product/{}/exchange'
@@ -13,7 +14,67 @@ class TestProductResource():
 
     def test_create_product_endpoint(self, test_client, session, teardown):
         def should_create_a_product(test_client, session):
-            pass
+            user = utils.create_resource(
+                session,
+                User,
+                **{
+                    'document': '00.000.000/0000-00',
+                    'email': 'danilolmoura@gmail.com',
+                    'name': 'Danilo da Silva',
+                    'password': '12345'
+                }
+            )
+
+            store_data = {
+                "name": "Loja do Danilo",
+                "coverage_area": [
+                        [
+                            [-22.885576, -43.276030],
+                            [-22.904009, -43.297860],
+                            [-22.910643, -43.271918]
+                        ]
+                    ],
+                    "location": {
+                            "lat": -22.894507,
+                            "lng": -43.260745
+                    },
+                    "user": user.id
+            }
+
+            res = test_client.post(
+                self.url_store,
+                data=json.dumps(store_data),
+                headers=utils.get_headers())
+
+            assert res.status_code == 200
+            store = json.loads(res.data)
+
+            data_product = {
+                'category': 1,
+                'description': 'Bola de vôlei 2 anos de uso',
+                'image_urls': ['https://cdn.ecvol.com/s/www.querocase.com.br/produtos/topsocket-bola-de-volei/z/0.png'],
+                'is_active': True,
+                'is_for_exchange': True,
+                'is_for_sale': False,
+                'name': 'Bola de vôlei',
+                'store': store.get('$id'),
+            }
+
+            res = test_client.post(
+                self.url_product,
+                data=json.dumps(data_product),
+                headers=utils.get_headers())
+
+            assert res.status_code == 200
+
+            product_json = json.loads(res.data)
+            assert data_product['category'] == product_json['category']
+            assert data_product['description'] == product_json['description']
+            assert data_product['image_urls'] == product_json['image_urls']
+            assert data_product['is_active'] == product_json['is_active']
+            assert data_product['is_for_exchange'] == product_json['is_for_exchange']
+            assert data_product['is_for_sale'] == product_json['is_for_sale']
+            assert data_product['name'] == product_json['name']
 
         should_create_a_product(test_client, session)
 
--- tests/utils.py
+++ tests/utils.py
@@ -4,3 +4,17 @@ def get_headers(token=None):
     }
 
     return headers
+
+def insert_object(session, obj):
+    try:
+        session.add(obj)
+        session.commit()
+        return obj
+    except Exception as e:
+        session.rollback()
+        raise e
+
+def create_resource(session, model, **kwargs):
+    obj = model(**kwargs)
+
+    return insert_object(session, obj)
